b208f7c3482d61a2784f77b6579396408fdeba58
Hassan Abedi <hassan.abedi.t@gmail.com>
2024-12-11 21:01:29 +0100

    Add a new script

---
+++ scripts/self_destruct.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "🚨 SYSTEM SELF-DESTRUCT SEQUENCE INITIATED 🚨"
+
+for i in {10..1}; do
+    echo "Self-destruct in $i..."
+    sleep 1
+done
+
+echo "💥💥💥"
+echo "Just kidding! Your system is safe. 😜"
+echo "Have a great day!"
